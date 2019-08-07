626dfa7ff1bc906deea8af6d4edf625385e41eab
Wandi <wandifrog@gmail.com>
2019-08-07 14:04:37 +0700

    cp3

--- lib/screens/Home/Main_PageView/insta_stories.dart
+++ lib/screens/Home/Main_PageView/insta_stories.dart
@@ -4,20 +4,41 @@ class InstaStories extends StatelessWidget {
   final stories = ListView(
     scrollDirection: Axis.horizontal,
     children: <Widget>[
-      Text('1'),
-      Text('1'),
-      Text('1'),
-      Text('1'),
-      Text('1'),
+      Container(
+        width: 160.0,
+        height: 160.0,
+        color: Colors.red,
+      ),
+      Container(
+        width: 160.0,
+        height: 160.0,
+        color: Colors.red,
+      ),
     ],
   );
+
+  dummyStories(MaterialColor _color) {
+    return Container(
+      width: 60.0,
+      margin: EdgeInsets.only(right: 10),
+      foregroundDecoration:
+          BoxDecoration(shape: BoxShape.circle, color: _color),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Container(
       margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-      // margin: const EdgeInsets.all(16.0),
-      child: Text('baba bi')
+      height: 60.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          dummyStories(Colors.green),
+          dummyStories(Colors.red),
+          dummyStories(Colors.blue),
+        ],
+      ),
     );
   }
 }
